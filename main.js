615efe006cb9211e020c90f77cb81b8193ac57c8
D3ATHSCYTH3R <94089013+D3ATHSCYTH3R@users.noreply.github.com>
2022-06-29 09:27:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(550, 400);
+canvas.position(340, 150);
+c="";
+}
+function draw(){
+    circle(30, 30, 20);
+    circle(410,350,30);
+    ellipse(340, 180, 55, 55);
+    recta=rect(340, 170, 120, 120, 20);
+    
+}
